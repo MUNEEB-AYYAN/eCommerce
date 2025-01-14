7f2c874149ca8a8485eb8f61d6dbdb5d69f8ea73
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-01-15 01:31:05 +0530

    initial

---
+++ eCommerceM/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-orange-600 text-white text-3xl font-bold py-4 flex justify-center'>Hello Yaaro</h1>
+    </>
+  )
+}
+
+export default App
